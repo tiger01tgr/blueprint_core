dc41d4965a925a9f5a79013ed07dfe7d900a794f
Tiger Li <tiger01tgr@gmail.com>
2023-10-13 15:01:42 -0500

    stuff

--- cmd/init/main.go
+++ cmd/init/main.go
@@ -12,21 +12,20 @@ import (
 )
 
 func main() {
-
 	var env string
-	if os.Getenv("ENV") == "prod" {
-		env = ".env"
-	} else if os.Getenv("ENV") == "dev" {
-		env = ".env.dev"
-	} else if os.Getenv("ENV") == "deploy" {
-		
+	if os.Getenv("ENV") != "deploy" {
+		if os.Getenv("ENV") == "prod" {
+			env = ".env"
+		} else if os.Getenv("ENV") == "dev" {
+			env = ".env.dev"
+		}
+		err := godotenv.Load(env)
+		if err != nil {
+			panic("Error loading .env file")
+		}
+		log.Println(env)
 	}
 
-	err := godotenv.Load(env)
-	if err != nil {
-		panic("Error loading .env file")
-	}
-	log.Println(env)
 	db.GetDB()
 	defer db.GetDB().Close()
 	firebase.GetFirebase()
